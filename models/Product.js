6760e6f2c760c27f57b857b68a7fe9dc3a84bed4
Jennifer Eckenrode <j.eckenrode@me.com>
2023-05-12 18:03:01 -0500

    added exact decimal

--- models/Product.js
+++ models/Product.js
@@ -18,7 +18,7 @@ Product.init(
       allowNull: false
     },
     price: {
-      type: DataTypes.DECIMAL,
+      type: DataTypes.DECIMAL(10,2),
       allowNull: false,
       validate: {
         isDecimal: true
@@ -30,6 +30,13 @@ Product.init(
       defaultValue: 10,
       validate: {
         isNumeric: true
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: 'category',
+        key: 'id'
       }
     }
   },
